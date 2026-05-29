6aac92fc927524443083b3fdf0e8a6d6c52243c9
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-29 20:11:17 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class clusterCalidateWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = clusterCalidateWorker()
+    print("Worker engine initialized.")
